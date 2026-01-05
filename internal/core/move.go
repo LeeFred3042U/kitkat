865d9807f687e85840c039213751320d97e0bf37
Harshavardhan <harshath3018@gmail.com>
2026-01-05 18:18:50 +0530

    fix: Fixed silent data loss by kitkat mv (#106)

--- internal/core/move.go
+++ internal/core/move.go
@@ -1,12 +1,31 @@
 package core
 
 import (
+	"errors"
 	"os"
 
 	"github.com/LeeFred3042U/kitkat/internal/storage"
 )
 
-func MoveFile(oldPath, newPath string) error {
+func MoveFile(oldPath, newPath string, force bool) error {
+	if oldPath == newPath {
+		return errors.New("source and destination paths are the same")
+	}
+
+	// If force is true, overwrites destination
+	// If not returns error if destination path already exists
+	if force {
+		if err := os.RemoveAll(newPath); err != nil && !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		if _, err := os.Stat(newPath); err == nil {
+			return errors.New("destination path already exists")
+		} else if !os.IsNotExist(err) {
+			return err
+		}
+	}
+
 	// Rename file
 	if err := os.Rename(oldPath, newPath); err != nil {
 		return err
